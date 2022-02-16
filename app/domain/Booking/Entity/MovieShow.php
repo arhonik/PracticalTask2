4883bd8742d454f02294b60c0a7ac5d7ca326ba8
arhonik <kotnyaa@gmail.com>
2022-02-16 15:40:17 +0400

    IS-2 Update class

--- app/domain/Booking/Entity/MovieShow.php
+++ app/domain/Booking/Entity/MovieShow.php
@@ -11,43 +11,30 @@ class MovieShow
     private DateTimeOfMovie $dateTimeOfMovie;
     private int $numberOfTicket;
 
-    public function getId(): int
-    {
-        return $this->id;
-    }
-
-    public function setId(int $id): void
-    {
+    public function __construct(
+        int $id,
+        Movie $movie,
+        DateTimeOfMovie $dateTimeOfMovie,
+        int $numberOfTicket
+    ) {
         $this->id = $id;
+        $this->movie = $movie;
+        $this->dateTimeOfMovie = $dateTimeOfMovie;
+        $this->numberOfTicket = $numberOfTicket;
     }
 
     public function getMovie(): Movie
     {
         return $this->movie;
     }
-
-    public function setMovie(Movie $movie): void
-    {
-        $this->movie = $movie;
-    }
-
+    
     public function getDateTimeOfMovie(): DateTimeOfMovie
     {
         return $this->dateTimeOfMovie;
     }
 
-    public function setDateTimeOfMovie(DateTimeOfMovie $dateTimeOfMovie): void
-    {
-        $this->dateTimeOfMovie = $dateTimeOfMovie;
-    }
-
     public function getNumberOfTicket(): int
     {
         return $this->numberOfTicket;
-    }
-
-    public function setNumberOfTicket(int $numberOfTicket): void
-    {
-        $this->numberOfTicket = $numberOfTicket;
     }
 }
