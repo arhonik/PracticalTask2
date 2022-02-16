3b4b87861c019dd654fa8d1ef5964cb660aee6c9
arhonik <kotnyaa@gmail.com>
2022-02-16 10:03:31 +0400

    IS-2 Update Ticket

--- app/domain/Booking/Entity/Ticket.php
+++ app/domain/Booking/Entity/Ticket.php
@@ -4,5 +4,15 @@ namespace App\Domain\Booking\Entity;
 
 class Ticket
 {
+    private int $id;
+    private string $clientName;
+    private string $phoneClient;
+    private string $film;
+    private string $date;
+    private string $duration;
+
+    public function __construct()
+    {
+    }
 
 }
