d50ae2bdb76b04600da1d0ed82b551ee7a90ba3f
arhonik <kotnyaa@gmail.com>
2022-02-16 08:44:16 +0400

    IS-2 Update gitignore

--- app/domain/Booking/Entity/TransferObject/BookingDto.php
+++ app/domain/Booking/Entity/TransferObject/BookingDto.php
@@ -2,7 +2,28 @@
 
 namespace App\Domain\Booking\Entity\TransferObject;
 
+use http\Exception\InvalidArgumentException;
+
 class BookingDto
 {
+    public string $name;
+    public string $phone;
+    public int $movieShow;
+
+    public function load(array $data)
+    {
+        if (!is_array($data)) {
+            throw new InvalidArgumentException();
+        }
+
+        $this->name = $data["name"];
+        $this->phone = $data["phone"];
+        $this->movieShow = $data["movieShow"];
+    }
+
+    public static function fromRequest($request)
+    {
+        return (new self())->load($request->post());
+    }
 
 }
