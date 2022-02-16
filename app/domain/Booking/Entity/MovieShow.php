5d5b118285ee334bd10018a4364e68600d7a64dc
arhonik <kotnyaa@gmail.com>
2022-02-16 11:50:08 +0400

    IS-2 Update MovieShow

--- app/domain/Booking/Entity/MovieShow.php
+++ app/domain/Booking/Entity/MovieShow.php
@@ -13,8 +13,63 @@ class MovieShow
     private string $endTime;
     private string $duration;
 
-    public function fromDto(MovieShowDto $movieShowDto)
+    public function setId(int $id): void
     {
+        $this->id = $id;
+    }
 
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setMovieTitle(string $movieTitle): void
+    {
+        $this->movieTitle = $movieTitle;
+    }
+
+    public function getMovieTitle(): string
+    {
+        return $this->movieTitle;
+    }
+
+    public function setShowDate(string $showDate): void
+    {
+        $this->showDate = $showDate;
+    }
+
+    public function getShowDate(): string
+    {
+        return $this->showDate;
+    }
+
+    public function setStartTime(string $startTime): void
+    {
+        $this->startTime = $startTime;
+    }
+
+    public function getStartTime(): string
+    {
+        return $this->startTime;
+    }
+
+    public function setEndTime(string $endTime): void
+    {
+        $this->endTime = $endTime;
+    }
+
+    public function getEndTime(): string
+    {
+        return $this->endTime;
+    }
+
+    public function setDuration(string $duration): void
+    {
+        $this->duration = $duration;
+    }
+
+    public function getDuration(): string
+    {
+        return $this->duration;
     }
 }
