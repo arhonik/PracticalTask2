44fa7d69fb658e31b6de9a648e12fa4a3e376a88
arhonik <kotnyaa@gmail.com>
2022-02-16 14:20:02 +0400

    IS-2 Update class

--- app/domain/Booking/Entity/Ticket.php
+++ app/domain/Booking/Entity/Ticket.php
@@ -12,9 +12,53 @@ class Ticket
     private string $date;
     private string $duration;
 
-    public function __construct()
+    public function getId(): int
     {
-
+        return $this->id;
     }
 
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getClient(): Client
+    {
+        return $this->client;
+    }
+
+    public function setClient(Client $client): void
+    {
+        $this->client = $client;
+    }
+
+    public function getMovie(): string
+    {
+        return $this->movie;
+    }
+
+    public function setMovie(string $movie): void
+    {
+        $this->movie = $movie;
+    }
+
+    public function getDuration(): string
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(string $duration): void
+    {
+        $this->duration = $duration;
+    }
+
+    public function getDate(): string
+    {
+        return $this->date;
+    }
+
+    public function setDate(string $date): void
+    {
+        $this->date = $date;
+    }
 }
