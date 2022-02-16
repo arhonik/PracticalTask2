ff36466d354ece0cfed028c4bef39d3e36087103
arhonik <kotnyaa@gmail.com>
2022-02-16 11:56:13 +0400

    IS-2 Add Movie

---
+++ app/domain/Booking/Entity/Movie.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Domain\Booking\Entity;
+
+class Movie
+{
+    private int $id;
+    private string $title;
+    private string $duration;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getDuration(): string
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(string $duration): void
+    {
+        $this->duration = $duration;
+    }
+}
