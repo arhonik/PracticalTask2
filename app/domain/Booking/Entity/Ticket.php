ae893ee67c0afba4515d02d69a16c95938d75e89
arhonik <kotnyaa@gmail.com>
2022-02-16 10:56:43 +0400

    IS-2 Update class Ticket

--- app/domain/Booking/Entity/Ticket.php
+++ app/domain/Booking/Entity/Ticket.php
@@ -2,11 +2,12 @@
 
 namespace App\Domain\Booking\Entity;
 
+use App\Domain\Booking\Entity\ValueObject\Client;
+
 class Ticket
 {
     private int $id;
-    private string $clientName;
-    private string $phoneClient;
+    private Client $client;
     private string $movie;
     private string $date;
     private string $duration;
