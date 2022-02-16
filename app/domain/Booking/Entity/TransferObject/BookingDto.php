96c7f4811484538951a1da3c7841771499201af1
arhonik <kotnyaa@gmail.com>
2022-02-16 09:51:07 +0400

    IS-2 Update BookingDto

--- app/domain/Booking/Entity/TransferObject/BookingDto.php
+++ app/domain/Booking/Entity/TransferObject/BookingDto.php
@@ -2,28 +2,21 @@
 
 namespace App\Domain\Booking\Entity\TransferObject;
 
-use http\Exception\InvalidArgumentException;
-
 class BookingDto
 {
-    public string $name;
-    public string $phone;
-    public int $movieShow;
+    private string $name;
+    private string $phone;
+    private int $movieShow;
 
-    public function load(array $data)
+    public function load(?array $data)
     {
         if (!is_array($data)) {
-            throw new InvalidArgumentException();
+            throw new \InvalidArgumentException('Error type');
         }
 
         $this->name = $data["name"];
         $this->phone = $data["phone"];
         $this->movieShow = $data["movieShow"];
-    }
-
-    public static function fromRequest($request)
-    {
-        return (new self())->load($request->post());
     }
 
 }
