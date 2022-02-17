0d5247ff65830c140db585f9bb9f3e2090d21808
arhonik <kotnyaa@gmail.com>
2022-02-17 09:28:55 +0400

    IS-2 Fix type property

--- app/domain/Booking/Entity/TransferObject/MovieShowDto.php
+++ app/domain/Booking/Entity/TransferObject/MovieShowDto.php
@@ -4,9 +4,9 @@ namespace App\Domain\Booking\Entity\TransferObject;
 
 class MovieShowDto
 {
-    public int $title;
-    public int $date;
-    public int $startTime;
+    public string $title;
+    public string $date;
+    public string $startTime;
 
     public function load(?array $data)
     {
