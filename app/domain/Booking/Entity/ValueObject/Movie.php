08682214877399a5df7e6220f9c2c8218778d71e
arhonik <kotnyaa@gmail.com>
2022-02-17 08:31:02 +0400

    IS-2 Refactor Movie

--- app/domain/Booking/Entity/ValueObject/Movie.php
+++ app/domain/Booking/Entity/ValueObject/Movie.php
@@ -1,16 +1,14 @@
 <?php
 
-namespace App\Domain\Booking\Entity;
+namespace App\Domain\Booking\Entity\TransferObject;
 
 class Movie
 {
-    private int $id;
     private string $title;
     private string $duration;
 
-    public function __construct(int $id, string $title, string $duration)
+    public function __construct(string $title, string $duration)
     {
-        $this->id = $id;
         $this->title = $title;
         $this->duration = $duration;
     }
