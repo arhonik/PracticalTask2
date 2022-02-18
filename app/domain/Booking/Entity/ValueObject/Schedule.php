c249898b5d9b3fe2e4072467644a9564fa3142e1
arhonik <kotnyaa@gmail.com>
2022-02-18 22:38:22 +0400

    IS-2 Refactor class

--- app/domain/Booking/Entity/ValueObject/Schedule.php
+++ app/domain/Booking/Entity/ValueObject/Schedule.php
@@ -2,24 +2,29 @@
 
 namespace App\Domain\Booking\Entity\ValueObject;
 
+use DateTimeImmutable;
+use DateTimeInterface;
+use DateTimeZone;
+use DomainException;
+
 class Schedule
 {
-    private \DateTimeInterface $startAt;
-    private \DateTimeInterface $endAt;
+    private DateTimeInterface $startAt;
+    private DateTimeInterface $endAt;
 
     public function __construct(string $startTime, string $endTime)
     {
         self::acceptCanBeConvertStringToDate($startTime);
-        $this->startAt = new \DateTimeImmutable("$startTime", new \DateTimeZone("Europe/Moscow"));
+        $this->startAt = new DateTimeImmutable("$startTime", new DateTimeZone("Europe/Moscow"));
 
         self::acceptCanBeConvertStringToDate($endTime);
-        $this->endAt = new \DateTimeImmutable("$endTime", new \DateTimeZone("Europe/Moscow"));
+        $this->endAt = new DateTimeImmutable("$endTime", new DateTimeZone("Europe/Moscow"));
     }
 
     private static function acceptCanBeConvertStringToDate(string $string)
     {
         if (!strtotime($string)) {
-            throw new \DomainException('Can\'t convert string to date');
+            throw new DomainException('Can\'t convert string to date');
         }
     }
 
