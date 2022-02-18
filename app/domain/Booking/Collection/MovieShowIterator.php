dc06b89c74ed8cdf4bd8e422103938a97dd73638
arhonik <kotnyaa@gmail.com>
2022-02-18 22:14:27 +0400

    IS-2 Add Movie show iterator

---
+++ app/domain/Booking/Collection/MovieShowIterator.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Domain\Booking\Collection;
+
+use App\Domain\Booking\Entity\MovieShow;
+
+class MovieShowIterator implements \Iterator
+{
+    private MovieShowCollection $collection;
+    private int $position = 0;
+    private bool $reverse = false;
+
+    public function __construct(MovieShowCollection $collection, $reverse = false)
+    {
+        $this->collection = $collection;
+        $this->reverse = $reverse;
+    }
+
+    public function rewind()
+    {
+        $this->position = $this->reverse ?
+            count($this->collection->get()) - 1 : 0;
+    }
+
+    public function current(): MovieShow
+    {
+        return $this->collection->get()[$this->position];
+    }
+
+    public function key(): int
+    {
+        return $this->position;
+    }
+
+    public function next()
+    {
+        $this->position = $this->position + ($this->reverse ? -1 : 1);
+    }
+
+    public function valid(): bool
+    {
+        return isset($this->collection->get()[$this->position]);
+    }
+
+}
